dc84dae13653836411288c716fbc14dcc1bb54b7
muhammed salman <salmanmuhammed827@gmail.com>
2023-12-07 01:32:03 +0530

    first comit

---
+++ calc.js
@@ -0,0 +1,16 @@
+function displayData(e)
+{
+   result.value+=e
+}
+function out()
+{
+    result.value=eval(result.value)
+}
+function allClear()
+{
+    result.value=""
+}
+function Delete()
+{
+    result.value=result.value.slice(0,-1)
+}
